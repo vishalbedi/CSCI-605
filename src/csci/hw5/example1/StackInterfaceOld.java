6d07fcec4d829f2082fac0fefa540ab953852859
Vishal Bedi <vishal.b1989@gmail.com>
2015-09-27 17:25:40 -0400

    Comments added

---
+++ src/csci/hw5/example1/StackInterfaceOld.java
@@ -0,0 +1,8 @@
+package csci.hw5.example1;
+
+public interface StackInterfaceOld {
+    public void push( Object item );
+    public Object pop();
+    public Object peek();
+    public boolean isEmpty();
+}
---
+++ src/csci/hw5/example1/StringStackOld.java
@@ -0,0 +1,39 @@
+package csci.hw5.example1;
+
+//this implementation implements the methods,
+//but the methods are null methods;
+public class StringStackOld implements StackInterfaceOld {
+ 
+ public void push( Object item )	{	       }
+ public Object pop() 		{ return null; }
+ public Object peek() 		{ return "hi"; }
+ public boolean isEmpty() 		{ return true; }
+
+ public static void main(String args[])	{
+	StackInterfaceOld aStackInterfaceOld = new StringStackOld();
+	/*
+	 * Since the push method expects an object all the classes extends object class
+	 * thus strings can be pushed inside the stack 
+	 */
+	aStackInterfaceOld.push("hello");	// why is here no warning?
+	/*
+	 * The pop() method returns an object of the Object class. 
+	 * Since we are trying to store the value of returned by the pop method into
+	 * a String type variable we get an error of type mismatch.
+	 * If we cast the object of Object class into string this error will go away at compile time
+	 * if the inserted element was not a string it will result in an cast exception at run time.
+	 */
+	String aString = aStackInterfaceOld.pop();
+ }
+/*
+javac StringStackOld.java			// explain this error
+StringStackOld.java:11: incompatible types	// explain what a cast would do
+found   : java.lang.Object			// regarding possible compiler error detection
+required: java.lang.String
+	String aString = aStackInterfaceOld.pop();
+	                                       ^
+1 error
+
+*/
+
+}
