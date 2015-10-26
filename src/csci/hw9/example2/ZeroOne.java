4a50a132a2798b22eccaa926cf6f731164eec067
Vishal Bedi <vishal.b1989@gmail.com>
2015-10-26 12:48:33 -0400

    Example 2 done.

--- src/csci/hw9/example2/ZeroOne.java
+++ src/csci/hw9/example2/ZeroOne.java
@@ -1,27 +1,30 @@
 package csci.hw9.example2;
 
-
 public class ZeroOne extends Thread	{
 	private String info;
 	static Object o = new Object();
+	static long nextId = -10;
 	static boolean oneIsRunning = false;
-	static boolean twoIsRunning = false;
-					     
+	
 	public ZeroOne (String info) {
 		this.info    = info;
 	}
 	public void run () {
 		while ( true )	{
 			synchronized ( o ) {
-				o.notify();
-				System.out.print(info);
 				try {
-					if ( ! oneIsRunning )	{
-						( new ZeroOne("1") ).start();
-						oneIsRunning = true;
+					if ( nextId == -10)	{
+						nextId = Long.parseLong(this.info);
+						System.out.print(info);
+						for (int i = 1; i < 99; i++)
+						( new ZeroOne(""+i) ).start();
 					}
-					
-					sleep(300);
+					if(nextId + 1 == Long.parseLong(this.info)){
+						System.out.print(info);
+						nextId = nextId == 97 ? -1 : Long.parseLong(this.info);
+						o.notifyAll();
+					}
+					sleep(3);
 					o.wait();
 				} catch ( Exception e ) { }
 			}
